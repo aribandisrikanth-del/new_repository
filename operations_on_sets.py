d72f75cc07f276f8eee5bd565a17810165e2ef6b
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-03-05 20:46:38 -0500

    Add files via upload

---
+++ operations_on_sets.py
@@ -0,0 +1,12 @@
+my_set1={1,2,3,2,4,5,8,4,2,7,3,4,8,4,2}
+my_set2={1,True,"6","string",3.82,False,32,"good"}
+my_set3={1,2,3,4,3,2,4}
+my_set4={1,2,3,2}
+my_set5={0,1,3,4,5}
+print(my_set1)
+print(my_set2)
+print(my_set3)
+print(my_set4)
+for item in range(1,6):
+    print(item,end=",")
+print()
