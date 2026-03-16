615986b19fec81e11e67b5a3bdb34c9e01fb7da6
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-03-16 19:35:18 -0400

    Add files via upload

---
+++ class_parrot.py
@@ -0,0 +1,6 @@
+class parrot:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+clown_pierces_parrot=parrot("mace welder",600)
+print("clown pierce's parrot's name is",clown_pierces_parrot.name,"his age is",clown_pierces_parrot.age)
